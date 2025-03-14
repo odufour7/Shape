8683f8cc49883554ce29c872bb1e742adc4e528b
oscar Dufour <oscardufour@MacBook-Pro-de-oscar.local>
2025-03-14 15:01:44 +0100

    requirements

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,7 +7,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../.."))
+sys.path.insert(0, os.path.abspath("../../src"))
 print(os.path.abspath("../.."))
 
 # -- Project information -----------------------------------------------------
@@ -34,5 +34,5 @@ language = "english"
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
