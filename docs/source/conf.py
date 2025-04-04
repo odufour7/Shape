dca4dc1ca8e48af547dc9e0a67248614732d8ab7
oscar Dufour <oscardufour@MacBook-Pro-de-oscar.local>
2025-04-04 16:03:38 +0200

    rm release in conf.py sphinx doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ smartquotes = False
 project = "shapes"
 copyright = "2025, The SHAPE project"
 author = "Oscar Maxime Alexandre"
-release = "1.0.0"
+# release = "1.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
