f31fdbe7932435bf17210a91cce20373a50b5713
oscar Dufour <oscardufour@MacBook-Pro-de-oscar.local>
2025-05-05 23:23:44 +0200

    test pre commit cpp

---
+++ src/mechanical_layer/try.cpp
@@ -0,0 +1,10 @@
+// Copyright 2025 Your Name or Company
+
+#include <iostream>
+
+int add(int a, int b) { return a + b; }
+
+int main() {
+    std::cout << "Sum: " << add(2, 3) << std::endl;
+    return 0;
+}
